f9a87f0d8546f79bf6da9e509e8298904ee9f652
Olamide.Laleye <olamide.laleye@interswitchng.com>
2019-09-05 11:08:22 +0100

    Inflate Graph;

---
+++ app/src/main/java/com/olamide/cowryconvert/ui/views/GraphView.kt
@@ -0,0 +1,16 @@
+//package com.olamide.cowryconvert.ui.views
+//
+//import android.content.Context
+//import android.graphics.Canvas
+//import android.util.AttributeSet
+//import android.widget.ImageView
+//import android.widget.RelativeLayout
+//
+//class GraphView(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) :
+//    RelativeLayout(context, attrs, defStyleAttr, defStyleRes) {
+//
+//
+//    override fun onDraw(canvas: Canvas?) {
+//        super.onDraw(canvas)
+//    }
+//}
